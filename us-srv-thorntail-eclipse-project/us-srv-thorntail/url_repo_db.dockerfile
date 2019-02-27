a2ae0e5f83ddb73b3c2df5d967420c4364ab44b1
KAORURONALDO <KAORURONALDO@london>
2019-02-27 03:27:38 -0300

    Deleting to use another eclipse project structure;

--- us-srv-thorntail-eclipse-project/us-srv-thorntail/url_repo_db.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM mongo:4.0.6
-COPY ./url_repo_db.js /docker-entrypoint-initdb.d
-ENV MONGO_INITDB_DATABASE url_repo_db
--- us-srv-thorntail-eclipse-project/us-srv-thorntail/url_repo_db.js
+++
@@ -1,6 +0,0 @@
-
-db = db.getSiblingDB("url_repo_db");
-
-db.createCollection("UrlRepo");
-
-db.createCollection("Sequence");
